5fa7f4f033a1ab7fae0b8fc7d5c5d16ddeed046d
Abdulkader Safi <safi.abdulkader@gmail.com>
2025-12-29 16:24:12 +0300

    update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,13 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Napco Analytics Dashboard" />
+	<meta name="keywords" content="analytics, dashboard, napco" />
+	<meta name="author" content="Abdulkader Safi" />
+	<title>Napco Analytics Dashboard</title>
+	<meta name="robots" content="index, follow" />
+</svelte:head>
 {@render children()}
